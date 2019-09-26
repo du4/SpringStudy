514d335669126a6d728dd4ecc744ce19bf8066de
Aleksei Shkantov <ashkantov@qualitetsystem.ru>
2019-09-26 18:03:56 +0300

    new Spring lessons from youtube

---
+++ src/main/java/by/du4/study/TestSpring.java
@@ -0,0 +1,12 @@
+package by.du4.study;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+    }
+}
